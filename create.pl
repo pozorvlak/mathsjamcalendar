5bd4f199ca53244e53075c138cb164fd139a4386
Miles Gould <miles@assyrian.org.uk>
2011-05-24 14:02:35 +0100

    Create one-off iCal events.

--- create.pl
+++ create.pl
@@ -9,33 +9,25 @@ use Data::ICal::DateTime;
 use DateTime::Event::Recurrence;
 use 5.010;
 
-# my $calendar = Data::ICal->new();
-# my $vevent = Data::ICal::Entry::Event->new();
-# $vevent->add_properties(
-#     summary => "Glasgow MathsJam meeting",
-#     description => "Go to the pub and talk about maths.",
-#     dtstart => Date::ICal->new(
-#         year => 2011, month => 6, day => 21, hour => 19
-#     )->ical,
-#     dtend => Date::ICal->new(
-#         year => 2011, month => 6, day => 21, hour => 23
-#     )->ical,
-#     location => "The Counting House, St Vincent's Place, Glasgow"
-# );
-# $vevent->recurrence(
 my $start = DateTime->new( year => 2011, month => 6, day => 20 );
 my $end = DateTime->new( year => 2013 );
+
+my $calendar = Data::ICal->new();
 my $recurrence = DateTime::Event::Recurrence->monthly(
         weeks => -2, days => "tu"
-#    )
 );
-$recurrence->next($start);
-# $calendar->add_entry($vevent);
-# print $calendar->as_string;
 
 for my $dt ($recurrence->as_list( start => $start, end => $end)) {
+    my $vevent = Data::ICal::Entry::Event->new();
+    $vevent->add_properties(
+        summary => "Glasgow MathsJam meeting",
+        description => "Go to the pub and talk about maths.",
+        location => "The Counting House, St Vincent's Place, Glasgow"
+    );
     $dt->add( hours => 19 );
-    print $dt->iso8601;
+    $vevent->start($dt);
     $dt->add( hours => 4 );
-    say " - " . $dt->iso8601;
+    $vevent->end($dt);
+    $calendar->add_entry($vevent);
 }
+print $calendar->as_string;
