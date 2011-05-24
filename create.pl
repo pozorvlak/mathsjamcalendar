2b3beff1f5fa94992bfe6bdaea67965a86867208
Miles Gould <miles@assyrian.org.uk>
2011-05-24 13:52:29 +0100

    Created times using DateTime::Event::Recurrence.

--- create.pl
+++ create.pl
@@ -7,23 +7,35 @@ use Data::ICal::Entry::Event;
 use Date::ICal;
 use Data::ICal::DateTime;
 use DateTime::Event::Recurrence;
+use 5.010;
 
-my $calendar = Data::ICal->new();
-my $vevent = Data::ICal::Entry::Event->new();
-$vevent->add_properties(
-    summary => "Glasgow MathsJam meeting",
-    description => "Go to the pub and talk about maths.",
-    dtstart => Date::ICal->new(
-        year => 2011, month => 6, day => 21, hour => 19
-    )->ical,
-    dtend => Date::ICal->new(
-        year => 2011, month => 6, day => 21, hour => 23
-    )->ical,
-    location => "The Counting House, St Vincent's Place, Glasgow"
-);
-$vevent->recurrence(DateTime::Event::Recurrence->monthly(
+# my $calendar = Data::ICal->new();
+# my $vevent = Data::ICal::Entry::Event->new();
+# $vevent->add_properties(
+#     summary => "Glasgow MathsJam meeting",
+#     description => "Go to the pub and talk about maths.",
+#     dtstart => Date::ICal->new(
+#         year => 2011, month => 6, day => 21, hour => 19
+#     )->ical,
+#     dtend => Date::ICal->new(
+#         year => 2011, month => 6, day => 21, hour => 23
+#     )->ical,
+#     location => "The Counting House, St Vincent's Place, Glasgow"
+# );
+# $vevent->recurrence(
+my $start = DateTime->new( year => 2011, month => 6, day => 20 );
+my $end = DateTime->new( year => 2013 );
+my $recurrence = DateTime::Event::Recurrence->monthly(
         weeks => -2, days => "tu"
-    )
+#    )
 );
-$calendar->add_entry($vevent);
-print $calendar->as_string;
+$recurrence->next($start);
+# $calendar->add_entry($vevent);
+# print $calendar->as_string;
+
+for my $dt ($recurrence->as_list( start => $start, end => $end)) {
+    $dt->add( hours => 19 );
+    print $dt->iso8601;
+    $dt->add( hours => 4 );
+    say " - " . $dt->iso8601;
+}
